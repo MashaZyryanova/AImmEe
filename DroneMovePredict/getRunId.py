24dfcd7218b68bbb82a81bcd02c6084d78f73a8a
NRostova <mzyrianova14@gmail.com>
2019-08-07 23:12:16 -0700

    add remove key

--- DroneMovePredict/getRunId.py
+++ DroneMovePredict/getRunId.py
@@ -4,8 +4,8 @@ import firedrone.client as fdc
 from firedrone.client.errors import FireDroneClientHttpError
 
 
-workspace = fdc.Workspace('7#ogORpZ1j9CRAT$-AYVoG4SgVXnkjf&rC6Xg2kADk^ece-_gM9X5bp1HXA%%C!S' )
-#workspace.directrun_end('390c00b6-4cf8-4e0d-8c75-a5b43e38a26e')
+workspace = fdc.Workspace('' )
+#workspace.directrun_end('')
 
 #Insert scene number
 try:

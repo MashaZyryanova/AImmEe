a4b2f82b11e54c7daaf8ba115758f76c92c236c6
NRostova <mzyrianova14@gmail.com>
2019-08-07 18:21:04 -0700

    build folder for front-end

---
+++ build/precache-manifest.e816083bc27ae58f880371df5233a485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187161973b0455c9c7ac1683366f22fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea1fbc6c9e578011935",
+    "url": "/static/css/main.f282f3bf.chunk.css"
+  },
+  {
+    "revision": "395972d10e22e5e66b76",
+    "url": "/static/js/2.113374c8.chunk.js"
+  },
+  {
+    "revision": "1ea1fbc6c9e578011935",
+    "url": "/static/js/main.53ec4968.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
